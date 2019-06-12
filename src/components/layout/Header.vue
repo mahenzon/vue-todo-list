b5635c479c1a89b4c7508f03359335b37134a778
Suren Khorenyan <surenkhorenyan@gmail.com>
2019-06-12 13:58:04 +0300

    Create Header component

---
+++ src/components/layout/Header.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+  header.header
+    h1 TodoList
+    div#nav
+      router-link(to="/") Home |
+      router-link(to="/about") About
+</template>
+
+
+<script>
+export default {
+  name: 'Header',
+}
+</script>
+
+<style scoped>
+  .header {
+    background: #333;
+    color: #fff;
+    text-align: center;
+    padding: 10px;
+  }
+
+  .header a {
+    color: #fff;
+    padding-right: 5px;
+    text-decoration: none;
+  }
+</style>
