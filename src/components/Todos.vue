ef8e7b5c38f5b20d5f459d6f449d5a499e152cab
Suren Khorenyan <surenkhorenyan@gmail.com>
2019-06-12 13:55:18 +0300

    Create Todos view

---
+++ src/components/Todos.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+  div
+    div(v-bind:key="todo.id" v-for="todo in todos")
+      TodoItem(v-bind:todo="todo" v-on:del-todo="$emit('del-todo', todo.id)")
+</template>
+
+<script>
+import TodoItem from './TodoItem'
+
+export default {
+    name: 'Todos',
+    components: {
+      TodoItem,
+    },
+    props: ['todos'],
+}
+</script>
+
+<style scoped>
+
+</style>
