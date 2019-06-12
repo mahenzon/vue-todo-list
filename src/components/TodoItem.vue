66dad64cfa12fa6da73a7d0f116d0850848458a2
Suren Khorenyan <surenkhorenyan@gmail.com>
2019-06-12 13:54:53 +0300

    Create Todo Item

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,43 @@
+<template lang="pug">
+  div(v-bind:class="{'is-completed': todo.completed}").todo-item
+    p
+      input(type="checkbox" v-on:change="markComplete" v-bind="{checked: todo.completed}")
+      span {{ todo.title }}
+      button(@click="$emit('del-todo', todo.id)").del X
+</template>
+
+
+<script>
+export default {
+  name: 'TodoItem',
+  props: ['todo'],
+  methods: {
+    markComplete() {
+      this.todo.completed = !this.todo.completed
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+  .todo-item {
+    background: #f4f4f4;
+    padding: 10px;
+    border-bottom: 1px #ccc dotted;
+  }
+
+  .is-completed {
+    text-decoration: line-through;
+  }
+
+  .del {
+    background: #f00;
+    color: #fff;
+    border: none;
+    padding: 5px 9px;
+    border-radius: 50%;
+    cursor: pointer;
+    float: right;
+  }
+</style>
